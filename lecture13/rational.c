535d1e7d8178e97f59318cd972e2924ca32b0778
Allen Downey <downey@allendowney.com>
2014-03-10 09:54:45 -0400

    Adding lecture13

---
+++ lecture13/rational.c
@@ -0,0 +1,56 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+typedef struct {
+    int numer;
+    int denom;
+} Rational;
+
+// Returns a new Rational with the given numer and denom.
+// If unable to allocate, prints an error message and exits.
+Rational *make_rational(int numer, int denom) {
+    // FILL THIS IN
+    return NULL;
+}
+
+// Computes a floating-point approximation of a Rational.
+double rational_to_double(Rational *rational) {
+    // FILL THIS IN
+    return 0.0;
+}
+
+// Multiplies two rational numbers; returns a new Rational.
+Rational *mult_rational(Rational *r1, Rational *r2) {
+    // FILL THIS IN
+    return NULL;
+}
+
+// Frees a Rational.
+void free_rational(Rational *rational) {
+    free(rational);
+}
+
+// Prints a rational in fraction form.
+void print_rational(Rational *rational) {
+    printf("%d / %d\n", rational->numer, rational->denom);
+}
+
+int main(void)
+{
+    Rational *rational = make_rational(3, 7);
+    print_rational(rational);
+
+    double d = rational_to_double(rational);
+    printf("%lf\n", d);
+
+    Rational *square = mult_rational(rational, rational);
+    print_rational(square);
+	
+    free_rational(rational);
+    free_rational(square);
+
+    return EXIT_SUCCESS;
+}
